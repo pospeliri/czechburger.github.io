caf15978c684f16ab27fbf93b0998740e9ea758f
apospelov13 <small99@hotbox.ru>
2017-10-30 16:36:06 +0300

    v2

--- js/common.js
+++ js/common.js
@@ -1,78 +1,33 @@
 $(function() {
 
-  smoothScroll('.topmenu_link','.nav_link');
+  //add selector for smoothScrool
+  smoothScroll('.topmenu_link');
 
-
+  // function smoothScrool
   function smoothScroll(element) {
     $(element).on('click', function(event){
+      $('.topmenu_close').click();
       if (this.hash !== "") {
         event.preventDefault();
         var hash = this.hash;
         $('html, body').animate({
            scrollTop: $(hash).offset().top
         }, 900, function(){
-            // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
         });
-      } // End if
+      } 
     });
-  }
-
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-      // Store hash
-      var hash = this.hash;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-         scrollTop: $(hash).offset().top
-      }, 900, function(){
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-      });
-    } // End if
-  });
-
-
-
+  };// function smoothScrool END
+  
+  // function reducing the height navbar when scrolling
   $(window).scroll(function() {
     if ($(".navbar").offset().top > 70) {
-        //$(".mainlogo").css('visibility', 'hidden');
         $(".mainlogo_img").addClass('mainlogo_img-scroll');
         $(".header_navbar").addClass('header_navbar-scroll');
       } else {
         $(".mainlogo_img").removeClass('mainlogo_img-scroll');
         $(".header_navbar").removeClass('header_navbar-scroll');
     }
-  });
-
-  $(document).ready(function () {
-    $(".topmenu_link").click(function(event) {
-      $(".header_topmenu").collapse('hide');
-    });
-  });
-
-  /*
-  $(window).scroll(function() {
-    $(".animation").each(function(){
-      var pos = $(this).offset().top;
-      //var winTop = $(window).scrollTop();
-      var winTop = $(window).scrollTop();
-      console.log('pos:' + pos);
-      console.log('winTop:' + winTop);
-      if (pos > (winTop)/2) {
-        //alert('pos' + pos + 'winTop' + winTop);
-        $(this).addClass("animated fadeInUpBig");
-        //$(".fadeUp").removeClass("fadeUp").addClass("animated fadeInUpBig");
-        //$(".fadeDown").removeClass("fadeDown").addClass("animated fadeInDownBig");
-      }
-    });
-  });
-  */
-
-
+  });// function reducing the height navbar when scrolling END
 
 });
